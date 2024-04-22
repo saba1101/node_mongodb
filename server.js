3d2d51d08ae70e1483e8795da9cd8abeb4b1ea5a
saba2446 <sabakhara44@gmail.com>
2024-04-22 14:58:53 +0400

    add: headers & cors

--- server.js
+++ server.js
@@ -2,9 +2,31 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const productRouter = require("./routes/productRouter");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+  })
+);
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
 const config = {
   port: 2000,
   uri: "mongodb+srv://sabakhara44:test1234@cluster0.qg2jqeq.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0",
